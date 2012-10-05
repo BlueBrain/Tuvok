BlueBrain/Tuvok

commit 0fd4119a0929b5031f14b67d6a0a6922d6991c76

- fixed isosurface lighting for color volumes - implemented interval arithmetic for isosurface search improved
git-svn-id: https://code.sci.utah.edu/svn/Tuvok@2436 c36c8488-0289-0348-9b64-b301f74bd9a7

Shaders/Compose-Color-FS.glsl
@@ -39,10 +39,23 @@ uniform sampler2D texRayHitPos;    ///< the hitposition of the ray (alpha flags 
 uniform sampler2D texRayHitNormal; ///< the surface normal at the hit position
 
 uniform vec3 vLightAmbient;
+uniform vec3 vLightDiffuse;
+uniform vec3 vLightSpecular;
 uniform vec3 vLightDir;
 
 uniform vec2 vScreensize;      ///< the size of the screen in pixels
 uniform vec2 vProjParam;       ///< X = far / (far - near)  / Y = (far * near / (near - far))
+
+vec3 Lighting(vec3 vPosition, vec3 vNormal, vec3 vLightAmbient, vec3 vLightDiffuse, vec3 vLightSpecular) {
+  vNormal.z = abs(vNormal.z);
+
+  vec3 vViewDir    = normalize(vec3(0.0,0.0,0.0)-vPosition);
+  vec3 vReflection = normalize(reflect(vViewDir, vNormal));
+  return clamp(vLightAmbient+
+       vLightDiffuse*max(abs(dot(vNormal, -vLightDir)),0.0)+
+       vLightSpecular*pow(max(dot(vReflection, vLightDir),0.0),8.0), 0.0,1.0);
+}
+
 
 void main(void){
   // compute the coordinates to look up the previous pass
@@ -67,10 +80,11 @@ void main(void){
   float l = length(vNormal);
 
   vNormal.z = abs(vNormal.z);
-  vec3 vLightColor = vColor.rgb*(vLightAmbient + clamp(abs(dot(vNormal, -vLightDir)),0.0,1.0));
+
+  vec3 vLightColor = vec4(Lighting(vPosition.xyz, vNormal, vLightAmbient, vColor.rgb*vLightDiffuse, vLightSpecular),1.0);
 
   /// write result to fragment color
-  gl_FragColor    = vec4(vLightColor.x, vLightColor.y, vLightColor.z, 1.0);
+  gl_FragColor = vec4(vLightColor.x, vLightColor.y, vLightColor.z, 1.0);
 
   // compute non linear depth from linear eye depth
   gl_FragDepth = vProjParam.x + (vProjParam.y / -vPosition.z);

Shaders/GLTreeRaycaster-Method-iso-color.glsl
@@ -15,6 +15,22 @@ vec3 GetVolumeNormal(vec3 currentPoolCoords, vec3 sampleDelta) {
   return ComputeAlphaNormal(currentPoolCoords, sampleDelta, vDomainScale);
 }
 
+vec3 RefineIsosurface(in vec3 vRayDir, in vec3 vCurrentPos) {
+  vRayDir /= 2.0;
+  vCurrentPos -= vRayDir;
+  for (int i = 0; i < 5; i++) {
+    vRayDir /= 2.0;
+    float voxel = samplePool4(vCurrentPos).a;
+    if (voxel >= fIsoval) {
+      vCurrentPos -= vRayDir;
+    } else {
+      vCurrentPos += vRayDir;
+    }
+  }
+  return vCurrentPos;
+}
+
+
 /*
    For more information, please see: http://software.sci.utah.edu
 

Shaders/GLTreeRaycaster-Method-iso.glsl
@@ -15,6 +15,21 @@ vec3 GetVolumeNormal(vec3 currentPoolCoords, vec3 sampleDelta) {
   return ComputeNormal(currentPoolCoords, sampleDelta, vDomainScale);
 }
 
+vec3 RefineIsosurface(in vec3 vRayDir, in vec3 vCurrentPos) {
+  vRayDir /= 2.0;
+  vCurrentPos -= vRayDir;
+  for (int i = 0; i < 5; i++) {
+    vRayDir /= 2.0;
+    float voxel = samplePool(vCurrentPos);
+    if (voxel >= fIsoval) {
+      vCurrentPos -= vRayDir;
+    } else {
+      vCurrentPos += vRayDir;
+    }
+  }
+  return vCurrentPos;
+}
+
 
 /*
    For more information, please see: http://software.sci.utah.edu

Shaders/GLTreeRaycaster-iso.glsl
@@ -61,6 +61,8 @@ void TerminateRay(bool bOptimalResolution) {
 
 bool GetVolumeHit(vec3 currentPoolCoords, out vec4 color);
 vec3 GetVolumeNormal(vec3 currentPoolCoords, vec3 sampleDelta);
+vec3 RefineIsosurface(in vec3 vRayDir, in vec3 vCurrentPos);
+
 
 // go
 void main()
@@ -162,6 +164,7 @@ void main()
 
           // ray termination
           if (bHit) {
+            currentPoolCoords = RefineIsosurface(voxelSpaceDirection, currentPoolCoords);
             rayHitPos = mModelToEye * vec4(currentPos = (currentPoolCoords-normToPoolTrans)/normToPoolScale, 1);
             rayHitPos.a = color.r+1.0;
             rayHitNormal = mModelViewIT * vec4(GetVolumeNormal(currentPoolCoords, sampleDelta),0.0);

commit a740a6e06d4890c638862e09e192c04a8aa5cda0

Fix shader compilation bug.
We were assigning a vec4 to a vec3.  Broke in r2436.

Only appearing on some ATIs, it seems.  I guess the nvidia
compiler transparently casts it to a vec3.

git-svn-id: https://code.sci.utah.edu/svn/Tuvok@2470 c36c8488-0289-0348-9b64-b301f74bd9a7

Shaders/Compose-Color-FS.glsl
@@ -57,7 +57,7 @@ vec3 Lighting(vec3 vPosition, vec3 vNormal, vec3 vLightAmbient, vec3 vLightDiffu
 }
 
 
-void main(void){
+void main(void) {
   // compute the coordinates to look up the previous pass
   vec2 vFragCoords = vec2(gl_FragCoord.x / vScreensize.x , gl_FragCoord.y / vScreensize.y);
 
@@ -81,7 +81,8 @@ void main(void){
 
   vNormal.z = abs(vNormal.z);
 
-  vec3 vLightColor = vec4(Lighting(vPosition.xyz, vNormal, vLightAmbient, vColor.rgb*vLightDiffuse, vLightSpecular),1.0);
+  vec3 vLightColor = Lighting(vPosition.xyz, vNormal, vLightAmbient,
+                              vColor.rgb*vLightDiffuse, vLightSpecular);
 
   /// write result to fragment color
   gl_FragColor = vec4(vLightColor.x, vLightColor.y, vLightColor.z, 1.0);